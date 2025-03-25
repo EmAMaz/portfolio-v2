b6f2007ee10e5fe6a0205671c9ba29064f6eece5
emamaz <santiagomaza33@gmail.com>
2025-03-25 18:24:30 -0300

    mejoras varias

--- src/components/cardProject.tsx
+++ src/components/cardProject.tsx
@@ -1,66 +1,45 @@
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 type Props = {
-  projectId: string;
-  urlImg: string;
+  nameProject: string;
   urlLive: string;
   urlGithub: string;
+  technology:string;
 };
 
 export default function CardProject({
-  projectId,
-  urlImg,
+  nameProject,
   urlLive,
   urlGithub,
+  technology
 }: Props) {
   const { t } = useTranslation();
-  const [isHovered, setIsHovered] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-    console.log("Mouse entered the element");
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-    console.log("Mouse left the element");
-  };
   return (
-    <div
-      className="relative bg-gray-800 rounded-lg cursor-pointer"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <img
-        className={`rounded-lg w-full h-64 bg-gray-800 ${
-          isHovered ? "opacity-20" : ""
-        }`}
-        src={urlImg}
-        alt=""
-      />
-      <div
-        className={`flex flex-col text-center absolute top-0 w-full justify-center items-center h-full ${
-          isHovered ? "opacity-100" : "opacity-0"
-        }`}
-      >
-        <div className="justify-center flex flex-col items-center w-full">
-          <p className="text-white text-lg font-geist-ultra">
-            {t(`text-project-title-${projectId}`)}
-          </p>
-          <span className="text-white">
-            {t(`text-project-description-${projectId}`)}
-          </span>
-          <div className="gap-4 flex pt-4">
-            <button className="border-[2px] p-2 border-white text-white hover:border-gray-400 hover:text-gray-400">
-              <a href={urlLive}>{t("text-project-buttoncard1")}</a>
-            </button>
-            <button className="border-[2px] p-2 border-white text-white hover:border-gray-400 hover:text-gray-400">
-              <a href={urlGithub}>{t("text-project-buttoncard2")}</a>
-            </button>
+    <div className="flex-1">
+      <ul style={{ paddingLeft: "8px" }}>
+        <li className="border-b-2 border-zinc-700 p-6">
+          <div>
+            <div className="font-semibold text-base leading-none ">
+              <div className="flex gap-6">
+                <div className="border-t-2 border-b-2 rounded-md border-zinc-700 p-3 w-max">{nameProject}</div>
+              </div>
+            </div>
+            <ul className="list-disc mt-4">
+              <p className="font-bold">{t("text-features-technology")}</p>
+              <li>{technology}</li>
+            </ul>
+            <div className="flex gap-4 mt-6">
+              <button className="border p-2 inset-shadow-2xs shadow-md hover:shadow-emerald-500/50">
+                <a href={urlLive}>{t("text-view-live")}</a>
+              </button>
+              <button className="border p-2 inset-shadow-2xs shadow-md hover:shadow-emerald-500/50">
+                <a href={urlGithub}>{t("text-view-code")}</a>
+              </button>
+            </div>
           </div>
-        </div>
-      </div>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -6,35 +6,51 @@ export default function ProjectPage() {
   const { t } = useTranslation();
   return (
     <>
-    <HeaderComponent></HeaderComponent>
-    
-    <div className="flex flex-col items-center bg-white">
-      <h1 className="mb-4 self-center mt-10 bg-clip-text text-gradient-vercel gradient-text">
-        {t("text-projects-title")}
-      </h1>
-      <div className="flex items-center gap-4">
-        <div className="flex flex-col gap-4">
+      <HeaderComponent></HeaderComponent>
+
+      <div className="flex flex-col items-center bg-white">
+        <h1 className="mb-10 self-center mt-10 bg-clip-text text-gradient-vercel gradient-text">
+          {t("text-projects-title")}
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center gap-12 border mb-10 p-12 rounded-md shadow-lg">
           <CardProject
-            projectId="1"
+            nameProject="ValoApi"
             urlGithub="https://github.com/EmAMaz/valorant-api"
-            urlLive="https://emamaz.github.io/valorant-api/"
-            urlImg="/projects/valoapipage.png"
+            urlLive="https://darling-sunshine-df7b34.netlify.app/"
+            technology="Angular, Node.js, Taildwind, API Rest"
           ></CardProject>
           <CardProject
-            projectId="2"
+            nameProject="Juego del ahorcado"
             urlGithub="https://github.com/EmAMaz/juegodelahorcado"
             urlLive="https://emamaz.github.io/juegodelahorcado/"
-            urlImg="/projects/ahorcadopage.png"
+            technology="HTML, CSS, JS, Canva"
+          ></CardProject>
+          <CardProject
+            nameProject="Encriptador de texto"
+            urlGithub="https://github.com/EmAMaz/encriptador"
+            urlLive="https://emamaz.github.io/encriptador/"
+            technology="HTML, CSS, JS, Canva"
+          ></CardProject>
+          <CardProject
+            nameProject="Carta Online"
+            urlGithub=""
+            urlLive="https://shiny-youtiao-a54e42.netlify.app/"
+            technology="React, Redux, Tailwind"
+          ></CardProject>
+          <CardProject
+            nameProject="Web de Paises"
+            urlGithub="https://github.com/EmAMaz/encriptador"
+            urlLive="https://stellar-tanuki-401827.netlify.app/"
+            technology="React, Redux, Tailwind, API Rest"
+          ></CardProject>
+          <CardProject
+            nameProject="Generator de consejos"
+            urlGithub="https://github.com/EmAMaz/advice-generator-app"
+            urlLive="https://advice-gntor.netlify.app/"
+            technology="React, Typescript, Tailwind , Vite, API Rest"
           ></CardProject>
         </div>
-        <CardProject
-          projectId="3"
-          urlGithub="https://github.com/EmAMaz/encriptador"
-          urlLive="https://emamaz.github.io/encriptador/"
-          urlImg="/projects/encriptadorpage.png"
-        ></CardProject>
       </div>
-    </div>
     </>
   );
 }
