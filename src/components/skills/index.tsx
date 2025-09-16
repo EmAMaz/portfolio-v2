1f9e1d1f510c238caa304b261832d3212e45b53c
emamaz <santiagomaza33@gmail.com>
2025-09-16 19:42:46 -0300

    reordenamiento de iconos 'tech'

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -11,19 +11,19 @@ export default function SkillPage() {
             {t("text-skills-title")}
           </h1>
           <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 overflow-y-auto max-h-screen py-8">
-            <CardSkills imgUrl="/typescript.svg" titulo="Typescript" urlRef="https://www.typescriptlang.org/"/>
-            <CardSkills imgUrl="/reactlogo.svg" titulo="React" urlRef="https://reactjs.org/"/>
-            <CardSkills imgUrl="/angular.svg" titulo="Angular" urlRef="https://angular.dev/"/>
-            <CardSkills imgUrl="/nextjs.svg" titulo="Next.js" urlRef="https://nextjs.org/"/>
-            <CardSkills imgUrl="/node.svg" titulo="Node.js" urlRef="https://nodejs.org/en/"/>
-            <CardSkills imgUrl="/express.svg" titulo="Express" urlRef="https://expressjs.com/"/>
-            <CardSkills imgUrl="/mysql.svg" titulo="Mysql" urlRef="https://www.mysql.com/"/>
-            <CardSkills imgUrl="/mongodb.svg" titulo="MongoDB" urlRef="https://www.mongodb.com/"/>
-            <CardSkills imgUrl="/firebase.svg" titulo="Firebase" urlRef="https://firebase.google.com/"/>
-            <CardSkills imgUrl="/git.svg" titulo="Git" urlRef="https://git-scm.com/"/>
-            <CardSkills imgUrl="/github.svg" titulo="Github" urlRef="https://github.com/"/>
-            <CardSkills imgUrl="/nginx.svg" titulo="Nginx" urlRef="https://nginx.com/"/>
-            <CardSkills imgUrl="/redux.svg" titulo="Redux" urlRef="https://nginx.com/"/>
+            <CardSkills imgUrl="iconsTech/typescript.svg" titulo="Typescript" urlRef="https://www.typescriptlang.org/"/>
+            <CardSkills imgUrl="iconsTech/reactlogo.svg" titulo="React" urlRef="https://reactjs.org/"/>
+            <CardSkills imgUrl="iconsTech/angular.svg" titulo="Angular" urlRef="https://angular.dev/"/>
+            <CardSkills imgUrl="iconsTech/nextjs.svg" titulo="Next.js" urlRef="https://nextjs.org/"/>
+            <CardSkills imgUrl="iconsTech/node.svg" titulo="Node.js" urlRef="https://nodejs.org/en/"/>
+            <CardSkills imgUrl="iconsTech/express.svg" titulo="Express" urlRef="https://expressjs.com/"/>
+            <CardSkills imgUrl="iconsTech/mysql.svg" titulo="Mysql" urlRef="https://www.mysql.com/"/>
+            <CardSkills imgUrl="iconsTech/mongodb.svg" titulo="MongoDB" urlRef="https://www.mongodb.com/"/>
+            <CardSkills imgUrl="iconsTech/firebase.svg" titulo="Firebase" urlRef="https://firebase.google.com/"/>
+            <CardSkills imgUrl="iconsTech/git.svg" titulo="Git" urlRef="https://git-scm.com/"/>
+            <CardSkills imgUrl="iconsTech/github.svg" titulo="Github" urlRef="https://github.com/"/>
+            <CardSkills imgUrl="iconsTech/nginx.svg" titulo="Nginx" urlRef="https://nginx.com/"/>
+            <CardSkills imgUrl="iconsTech/redux.svg" titulo="Redux" urlRef="https://nginx.com/"/>
           </div>
         </div>
       </div>
