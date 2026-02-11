749c51c0ea05029eaee17a74af1a9d467c860336
emamaz <santiagomaza33@gmail.com>
2026-02-11 08:19:59 -0300

    border en las cards sacadas en bajas resoluciones

--- src/components/cardSkills.tsx
+++ src/components/cardSkills.tsx
@@ -7,11 +7,10 @@ type Props = {
 export default function CardSkills({imgUrl, titulo, urlRef}: Props) {
   return (
     <a rel="noopener noreferrer" target="_blank" href={urlRef} >
-      <div className="scale-75 md:scale-90 foundation_card__v7VKB transition ease-in-out delay-150 hover:-translate-y-3 hover:scale-103 hover:bg-white/40 duration-300 flex items-center justify-center space-x-4">
+      <div className="md:scale-90 md:foundation_card__v7VKB transition ease-in-out delay-150 hover:-translate-y-3 hover:scale-103 hover:bg-white/40 duration-300 flex items-center justify-center space-x-4">
         <div>
           <img src={imgUrl} alt="" />
         </div>
-        <p className="font-bold">{titulo}</p>
       </div>
     </a>
   );
