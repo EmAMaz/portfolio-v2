4250eb8251e814ad07df91c79d792921a57b2ef9
emamaz <santiagomaza33@gmail.com>
2026-02-10 22:00:57 -0300

    background de fondo sacado

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -1,23 +1,26 @@
 import { useTranslation } from "react-i18next";
 import CardExperience from "./CardExperience";
+import { getExperienceYears } from "../../utils/getExperienceYears";
 
 export default function ExperiencePage() {
   const { t } = useTranslation();
   return (
     <div className="font-geist-ultra">
-      <div>
-        <section className="min-h-screen bg-gradient-to-t from-black/10 to-white absolute top-0 left-0 w-screen justify-center flex items-center">
-          <div className="container flex justify-center items-center mx-auto px-4 overflow-y-auto h-full pt-8 pb-16">
-            <div className="flex flex-col lg:flex-col gap-8 h-screen">
-                <h2 className="bg-clip-text text-gradient-vercel gradient-text select-none text-5xl font-bold text-gray-800 mb-4">
-                  {t("text-experience-title")}
-                </h2>
-              <div className="text-center lg:text-left flex-1">
-                <div className="grid grid-cols-1 xl:grid-cols-2 gap-12 xl:gap-0">
+      <div className="flex flex-col items-center bg-white justify-center h-screen pt-16 pb-8">
+          <div className="flex justify-center items-center px-4 pt-8 pb-16 h-full">
+            <div className="flex flex-col gap-8 h-full">
+              <h2 className="bg-clip-text text-gradient-vercel gradient-text select-none text-5xl font-bold text-gray-800 mb-4">
+                {t("text-experience-title")}
+              </h2>
+
+              <div className="text-center lg:text-left overflow-y-auto xl:overflow-hidden">
+                <div className="grid gap-12 xl:grid-cols-2 xl:gap-0 ">
                   <div className="flex flex-col gap-5">
                     <p className="text-gray-600 xl:w-[600px] text-left">
                       <span className="font-bold bg-gradient-to-r text-lg/7 from-black to-blue-400 bg-clip-text text-transparent">
-                        {t("text-experience-description")}
+                        {t("text-experience-description", {
+                          year: getExperienceYears(),
+                        })}
                       </span>
                     </p>
                     <div className="flex items-center gap-6">
@@ -40,17 +43,15 @@ export default function ExperiencePage() {
                       </div>
                     </div>
                   </div>
-                  <div>
-                    <CardExperience experience="freelance" />
-                    <CardExperience className="h-2/3 md:h-3/4 mb-6 xl:min-h-0" experience="venedicto" />
+                  <div className="flex flex-col xl:overflow-y-auto xl:h-3/4 xl:max-h-[625px]">
+                    <CardExperience className="flex-1" experience="forit" />
+                    <CardExperience className="flex-1" experience="freelance" />
+                    <CardExperience className="flex-1" experience="venedicto" />
                   </div>
                 </div>
-                
               </div>
-              
             </div>
           </div>
-        </section>
       </div>
     </div>
   );
