872f572eec63bd46f25ad6cbf450417b23f49487
emamaz <santiagomaza33@gmail.com>
2026-02-11 08:32:02 -0300

    Update cardSkills.tsx

--- src/components/cardSkills.tsx
+++ src/components/cardSkills.tsx
@@ -7,10 +7,11 @@ type Props = {
 export default function CardSkills({imgUrl, titulo, urlRef}: Props) {
   return (
     <a rel="noopener noreferrer" target="_blank" href={urlRef} >
-      <div className="md:scale-90 md:foundation_card__v7VKB transition ease-in-out delay-150 hover:-translate-y-3 hover:scale-103 hover:bg-white/40 duration-300 flex items-center justify-center space-x-4">
+      <div className="sm:scale-90 sm:foundation_card__v7VKB transition ease-in-out delay-150 hover:-translate-y-3 hover:scale-103 hover:bg-white/40 duration-300 flex items-center justify-center space-x-4">
         <div>
           <img src={imgUrl} alt="" />
         </div>
+        <p className="font-bold text-black/80 hidden sm:block">{titulo}</p>
       </div>
     </a>
   );
