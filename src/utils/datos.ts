102a03c464e1dd5aea7980b3e6feb005e7749d96
emamaz <santiagomaza33@gmail.com>
2026-02-13 17:31:05 -0300

    datos de proyectos almacenados en una variable

--- src/utils/datos.ts
+++ src/utils/datos.ts
@@ -12,3 +12,76 @@ export const enlaces = [
     iconImage: "linkedIn.svg",
   },
 ];
+
+export const projects = [
+  {
+    id: 1,
+    title: "ValoApi",
+    description:
+      "Angular, Node.js, Taildwind, API Rest",
+    image:
+      "/project/valoapipage.png",
+    demoLink: "https://darling-sunshine-df7b34.netlify.app/",
+    repoLink: "https://github.com/EmAMaz/valorant-api",
+  },
+  {
+    id: 2,
+    title: "Next Marvel Movies",
+    description:
+      "PHP, Picocss, Curl (consumo de API), API Rest",
+    image:
+      "/project/nextmarvelmoviepage.png",
+    demoLink: "https://santimazadev.rf.gd/next-marvel-movies/",
+    repoLink: "https://github.com/EmAMaz/next-marvel-movies",
+  },
+  {
+    id: 3,
+    title: "Juego del ahorcado",
+    description:
+      "Red social enfocada en comunidades creativas con chat en vivo y perfiles dinámicos.",
+    image:
+      "/project/ahorcadopage.png",
+    demoLink: "https://emamaz.github.io/juegodelahorcado/",
+    repoLink: "https://github.com/EmAMaz/juegodelahorcado",
+  },
+  {
+    id: 4,
+    title: "Encriptador de texto",
+    description:
+      "HTML, CSS, JS, Canva",
+    image:
+      "/project/encriptadorpage.png",
+    demoLink: "https://emamaz.github.io/encriptador/",
+    repoLink: "https://github.com/EmAMaz/encriptador",
+  },
+  {
+    id: 5,
+    title: "Carta Online",
+    description:
+      "React, Redux, Tailwind",
+    image:
+      "/project/cartaonlinepage.png",
+    demoLink: "https://shiny-youtiao-a54e42.netlify.app/",
+    repoLink: "#",
+  },
+  {
+    id: 6,
+    title: "Remix MP4",
+    description:
+      "Html, Css, Angular, SCSS",
+    image:
+      "/project/remixmp4page.png",
+    demoLink: "https://flourishing-longma-852c60.netlify.app/",
+    repoLink: "https://github.com/EmAMaz/mockUp-remixmp4",
+  },
+    {
+    id: 6,
+    title: "Generator de consejos",
+    description:
+      "React, Typescript, Tailwind , Vite, API Rest",
+    image:
+      "/project/generatorpage.png",
+    demoLink: "https://advice-gntor.netlify.app/",
+    repoLink: "https://github.com/EmAMaz/advice-generator-app",
+  }, 
+];
